1926d5c6ec81cd77a453f941eed7a7ecc81ce23c
Sébastien CHAZALLET <s.chazallet@gmail.com>
2014-12-09 21:10:25 +0100

    WSGI Configuration revisited

--- openerp-wsgi.py
+++ openerp-wsgi.py
@@ -1,23 +1,27 @@
-# WSGI Handler sample configuration file.
-#
-# Change the appropriate settings below, in order to provide the parameters
-# that would normally be passed in the command-line.
-# (at least conf['addons_path'])
-#
-# For generic wsgi handlers a global application is defined.
-# For uwsgi this should work:
-#   $ uwsgi_python --http :9090 --pythonpath . --wsgi-file openerp-wsgi.py
-#
-# For gunicorn additional globals need to be defined in the Gunicorn section.
-# Then the following command should run:
-#   $ gunicorn openerp:service.wsgi_server.application -c openerp-wsgi.py
+# -*- coding: utf-8 -*-
+
+"""
+WSGI Handler sample configuration file.
+=======================================
+
+Change the appropriate settings below, in order to provide the parameters
+that would normally be passed in the command-line.
+
+* For uwsgi this should work:
+
+    $ uwsgi_python --http :9090 --pythonpath . --wsgi-file openerp-wsgi.py
+
+For gunicorn additional globals need to be defined in the Gunicorn section.
+
+    $ gunicorn openerp:service.wsgi_server.application -c openerp-wsgi.py
+"""
 
 import openerp
 
 #----------------------------------------------------------
 # Common
 #----------------------------------------------------------
-openerp.multi_process = True # Nah!
+openerp.multi_process = True
 
 # Equivalent of --load command-line option
 openerp.conf.server_wide_modules = ['web']
@@ -28,8 +32,12 @@ conf = openerp.tools.config
 
 conf['addons_path'] = '/opt/odoo/addons,/opt/odoo/include/custom/8.0'
 
+#conf['admin_passwd'] = 'TrucSuperCompliquéEtVachementSecret'
+
 # Optional database config if not using local socket
-#conf['db_name'] = 'mycompany'
+#conf['db_name'] = 'TODO'
+#conf['dbfilter'] = 'TODO'
+#conf['list_db'] = 'TODO'
 #conf['db_host'] = 'localhost'
 #conf['db_user'] = 'foo'
 #conf['db_port'] = 5432
